076b8afc10d9ec42f3f2f17f802716e17fdb7f7d
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-08-15 14:27:19 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+
+const optionsForLightBox = {
+    captionsData: 'alt',
+    captionDelay: 250,
+}
+
+new SimpleLightbox('.gallery a', optionsForLightBox);
